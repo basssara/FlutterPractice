ef749ca889d1d7e51e86ed3004e16e1bef545491
Boburbek <40127110+basssara@users.noreply.github.com>
2021-10-15 17:24:12 +0500

    Add files via upload

---
+++ anotherpract/getX.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() => runApp(GetMaterialApp(home: Home()));
+
+class Controller extends GetxController {
+  var count = 0.obs;
+  increment() => count++;
+}
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final Controller c = Get.put(Controller());
+
+    return Scaffold(
+        appBar: AppBar(title: Obx(() => Text("Clicks: ${c.count}"))),
+        body: Center(
+            child: ElevatedButton(
+                child: Text("Go to Other"), onPressed: () => Get.to(Other()))),
+        floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.add), onPressed: c.increment));
+  }
+}
+
+class Other extends StatelessWidget {
+  // You can ask Get to find a Controller that is being used by another page and redirect you to it.
+  final Controller c = Get.find();
+
+  @override
+  Widget build(context) {
+    // Access the updated count variable
+    return Scaffold(body: Center(child: Text("${c.count}")));
+  }
+}
